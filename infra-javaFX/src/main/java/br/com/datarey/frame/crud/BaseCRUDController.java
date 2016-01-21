f5279c9366dfa0b8f7419831862b6ee2aad20b6c
henriquemem <henriquemem@gmail.com>
2016-01-21 08:35:01 -0200

    at

--- infra-javaFX/src/main/java/br/com/datarey/frame/crud/BaseCRUDController.java
+++ infra-javaFX/src/main/java/br/com/datarey/frame/crud/BaseCRUDController.java
@@ -1,5 +1,6 @@
 package br.com.datarey.frame.crud;
 
+import br.com.datarey.app.BaseReport;
 import br.com.datarey.context.Context;
 import br.com.datarey.controller.BaseController;
 import br.com.datarey.model.Entidade;
@@ -9,11 +10,15 @@ import br.com.datarey.util.MessageType;
 import br.com.datarey.util.MessageUtil;
 import br.com.generic.dao.SearchEntityListBuilder;
 import javafx.beans.property.SimpleListProperty;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
 import javafx.collections.ObservableList;
 import javafx.concurrent.Task;
+import javafx.concurrent.Worker;
 import javafx.fxml.FXML;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
+import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.scene.control.SelectionMode;
 import javafx.scene.control.TableColumn;
@@ -21,8 +26,19 @@ import javafx.scene.control.TableView;
 import javafx.scene.input.KeyCode;
 import javafx.scene.input.KeyEvent;
 import javafx.scene.layout.VBox;
+import javafx.scene.web.WebEngine;
+import javafx.scene.web.WebView;
+import javafx.stage.Stage;
+import org.w3c.dom.Element;
+import org.w3c.dom.html.HTMLObjectElement;
+import netscape.javascript.JSObject;
 
 import javax.inject.Inject;
+import javax.swing.text.html.HTMLDocument;
+import java.io.ByteArrayOutputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.lang.reflect.ParameterizedType;
 import java.lang.reflect.Type;
 import java.util.ArrayList;
@@ -51,6 +67,7 @@ public abstract class BaseCRUDController<E extends Entidade, S extends BaseServi
 
     private List<TableColumn<E, String>> columns = new ArrayList<>();
 
+
     @Override
     @FXML
     public void initialize() {
@@ -73,6 +90,43 @@ public abstract class BaseCRUDController<E extends Entidade, S extends BaseServi
     public void editColunmsKey(KeyEvent event){
         if(event.getCode() == KeyCode.ENTER){
             editColunmsAction();
+        }
+    }
+
+
+    @FXML
+    public void imprimirActionListener(){
+        ByteArrayOutputStream stream = getReport().exportToPDF(getReport().gerarRelatorio(table.getItems(), getReport().listarDicionario()));
+
+        WebView webView = new WebView();
+        WebEngine webEngine = webView.getEngine();
+        webEngine.getLoadWorker().stateProperty().addListener(( ov,  oldState, newState) ->{
+
+            if (newState == Worker.State.SUCCEEDED) {
+                JSObject document = (JSObject) webEngine.executeScript("document");
+                JSObject a = (JSObject) document.getMember("abrir");
+                document.call("abrir", stream.toByteArray());
+            }
+        });
+
+        webEngine.load(getClass().getResource("/br/com/datarey/PDFView/web/viewer.html").toString());
+
+        Stage stage = new Stage();
+
+        stage.setScene(new Scene(webView, 800, 1200));
+        stage.show();
+
+
+        java.io.File file = new java.io.File("C:\\log\\arquivo.pdf");
+        FileOutputStream in = null;
+        try {
+            in = new FileOutputStream(file);
+            in.write(stream.toByteArray());
+            in.close();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
         }
     }
 
@@ -177,4 +231,6 @@ public abstract class BaseCRUDController<E extends Entidade, S extends BaseServi
     public void setFormClass(Class<?> formClass) {
         this.formClass = formClass;
     }
+
+    protected abstract BaseReport getReport();
 }
